2a87122ed57df88fac0a608fcf66c7532f7068c0
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-27 11:37:06 +0300

    Visual Studion files

---
+++ CourseProject/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
